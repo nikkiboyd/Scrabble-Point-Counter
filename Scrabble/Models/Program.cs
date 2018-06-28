4d5d643464104383c1f08009eed2acaeaa0d079c
Nikki Boyd and Ryan Putman <nikki@>
2018-06-28 13:21:31 -0700

    add user interface to execute application

--- Scrabble/Models/Program.cs
+++ Scrabble/Models/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using Scrabble;
 
-namespace Scrabble
+namespace ScrabbleMain
 {
     class Program
     {
         static void Main()
         {
-            
+            Console.WriteLine("Enter a word to see the Scrabble points it is worth");
+            string userInput = Console.ReadLine();
+            ScorePoints ScorePoints = new ScorePoints();
+            Console.WriteLine(ScorePoints.GrabValues(userInput)); 
         }
     }
 }
--- Scrabble/Models/Scorer.cs
+++ Scrabble/Models/Scorer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ScrabbleMain;
 
 namespace Scrabble
 {
@@ -8,7 +9,7 @@ namespace Scrabble
     {
         Dictionary<char, int> dictionary = new Dictionary<char, int>() { { 'a', 1 }, { 'b', 3 }, { 'c', 3 }, { 'd', 2 }, { 'e', 1 }, { 'f', 4 }, { 'g', 2 }, { 'h', 4 }, { 'i', 1 }, { 'j', 8 }, { 'k', 5 }, { 'l', 1 }, { 'm', 3 }, { 'n', 1 }, { 'o', 1 }, { 'p', 3 }, { 'q', 10 }, { 'r', 1 }, { 's', 1 }, { 't', 1 }, { 'u', 1 }, { 'v', 4 }, { 'w', 4 }, { 'x', 8 }, { 'y', 4 }, { 'z', 10 }, };
 
-        public List<int> GrabValues(string userInput)
+        public int GrabValues(string userInput)
         {
             List<int> totalPoints = new List<int>();
 
@@ -19,8 +20,8 @@ namespace Scrabble
                   totalPoints.Add(dictionary[letter]);   
                 } 
             }
-            AddPoints(totalPoints);
-            return totalPoints;
+
+            return AddPoints(totalPoints);
         }
 
         public int AddPoints(List<int> totalPoints)
